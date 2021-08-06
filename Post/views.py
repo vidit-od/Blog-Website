603380f735ace1f025f958243343924ab67b42c6
VIDIT <vidit894@gmail.com>
2021-08-06 23:36:39 +0530

    Attach Login.html

--- Post/views.py
+++ Post/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'login.html')
