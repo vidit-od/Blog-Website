611b812846b92aa9545a084d2386f5c23ae79b0b
VIDIT <vidit894@gmail.com>
2021-08-06 23:41:32 +0530

    Attach SignUp.html

--- Post/urls.py
+++ Post/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns=[
     path('',views.index,name='index'),
     path('login',views.login,name='login'),
+    path('signup',views.signup,name='signup')
 ]
--- Post/views.py
+++ Post/views.py
@@ -6,3 +6,6 @@ def index(request):
 
 def login(request):
     return render(request, 'login.html')
+
+def signup(request):
+    return render(request, 'signup.html')
