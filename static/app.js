a6b6a9701c0c5b38aec42859ac9724ea00a26230
VIDIT <vidit894@gmail.com>
2021-08-09 17:30:18 +0530

    improving Js

--- static/app.js
+++ static/app.js
@@ -4,64 +4,67 @@ const content= document.querySelector('.content')
 const scroll_up=document.querySelector('.above')
 const scroll_down=document.querySelector('.below')
 const movement=document.querySelector('.movement')
+const hit_blogs=document.querySelector('.hit_blogs')
 let position=0
-
+let max_height=0
+let height_list=[]
 window.addEventListener('DOMContentLoaded',function(){
     // creating a list of all the sections in page
     // this list will be used for smooth scroll
     window.navigation=content.children
-    console.log(navigation)
-    movement.children[0].classList.add('remove')
-    scroll_down.innerHTML=`<a href="#${navigation[1].id}"><button> V </button></a>`
-    
+
+    scroll_up.innerHTML=`<div class="above"><a href="#top"><button> ^ </button></a></div>`
+
+    scroll_down.innerHTML=`<div class="above"><a href="#${navigation[1].id}"><button> V </button></a></div>`
+
+    offset_check()
+})
+
+// dynamic page offset allocation
+window.addEventListener('resize',function (){
+    offset_check()
 })
 window.addEventListener('scroll',function(){
-    window.pageYOffset
-    if(window.pageYOffset>=600){
-        movement.children[0].classList.remove('remove')
-    }
-    else{
-        movement.children[0].classList.add('remove')
+    offset_check()
+    for(i=0;i<navigation.length;i++){
+        if(Math.round(window.pageYOffset)<height_list[i]){
+        up=i-1
+        if(up==-1){
+            up=0
+        }
+        down=i+1
+        if(down==navigation.length){
+            down=navigation.length-1
+        }
+
+        // if(i==0){
+        //     scroll_up.classList.add('.remove')
+        // }
+        // else if(i==navigation.length-1){
+        //     scroll_down.classList.add('.remove')
+        // }
+        // else{
+        //     scroll_up.classList.remove('.remove')
+        //     scroll_down.classList.remove('.remove')    
+        // }
+        scroll_up.innerHTML=`<div class="above"><a href="#${navigation[up].id}"><button> ^ </button></a></div>`
+
+            
+        scroll_down.innerHTML=`<div class="above"><a href="#${navigation[down].id}"><button> V </button></a></div>`
+        return 0
+        }
     }
 })
-scroll_up.addEventListener('click',function(){
-    position=position-1
-    if(position<0){
-        position=0
+function offset_check(){
+    max_height=0
+    for(i=0;i<navigation.length;i++){
+        max_height=max_height+navigation[i].offsetHeight
+        height_list[i]=max_height
     }
-    console.log(navigation[position].id)
-    position_check()
     
-    scroll_down.innerHTML=`<a href="#${navigation[position].id}"><button> v </button></a>`
-
-    scroll_down.innerHTML=`<a href="#${navigation[position+1].id}"><button> v </button></a>`
-})
-
-scroll_down.addEventListener('click',function(){
-    position=position+1
-    if(position>navigation.length-1){
-        position=navigation.length-1
+    for(i=0;i<navigation.length;i++){
+        if(i!=0){
+            navigation[i].style.top=height_list[i-1]
+        }
     }
-    console.log(navigation[position].id)
-    position_check()
-    
-
-    scroll_up.innerHTML=`<a href="#${navigation[position-1].id}"><button> ^ </button></a>`
-    scroll_down.innerHTML=`<a href="#${navigation[position].id}"><button> v </button></a>`
-})
-function position_check(){
-    if (position==0){
-        movement.children[0].classList.add('remove')
-        movement.children[1].classList.remove('remove')
-    }
-    else if(position==navigation.length-1){
-        movement.children[0].classList.remove('remove')
-        movement.children[1].classList.add('remove')
-        
-    }
-    else{
-        movement.children[0].classList.remove('remove')
-        movement.children[1].classList.remove('remove')
-    }
-
 }
