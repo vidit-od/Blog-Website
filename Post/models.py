76db2085b5e31101be8d8defd08096f6a4cc2a4e
VIDIT <vidit894@gmail.com>
2021-08-11 10:38:35 +0530

    Adding a basic model

--- Post/models.py
+++ Post/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+class post(models.Model):
+    title= models.CharField(max_length=100)
+    date=models.DateField(default=datetime.now,blank=True)
+    body=models.CharField(max_length=10000)
