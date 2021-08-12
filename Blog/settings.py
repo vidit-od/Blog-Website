519181d7086b4ca15d13a1e95ddca7b719641a5e
VIDIT <vidit894@gmail.com>
2021-08-12 09:38:31 +0530

    Connecting Postgresql

--- Blog/settings.py
+++ Blog/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Blog',
+        'USER': 'postgres',
+        'PASSWORD':'vidit894',
+        'HOST' :'localhost',
     }
 }
 
