6b6c4e4a0708421174c54dfadd3e75ad565e6139
VIDIT <vidit894@gmail.com>
2021-08-23 12:26:13 +0530

    catagory filter

--- static/app_blog.js
+++ static/app_blog.js
@@ -1,7 +1,11 @@
 const catagory=document.querySelectorAll(".catagory")
 const catagory_select=document.querySelector(".catagory_select")
 let catagory_list=[]
-const submit=document.getElementById("submit")
+const form=document.querySelector(".form")
+const submit=document.querySelector(".submit")
+const blog=document.querySelectorAll(".blog")
+const blogs=document.querySelector(".blogs")
+
 window.addEventListener('DOMContentLoaded',function(){
     for (i=0;i<catagory.length;i++){
         if (i==0){
@@ -18,4 +22,31 @@ window.addEventListener('DOMContentLoaded',function(){
     }
     console.log(catagory_select.innerHTML)
     catagory_select.innerHTML=line
+})
+
+submit.addEventListener("click",function(){
+    const input_catagory= document.getElementById("catagory_select").value
+    const input_sort=document.getElementById("sort").value
+    const input_author=document.getElementById("author").value
+    let content=""
+    if(input_sort=="Latest at Top"){
+        for(i=0;i<blog.length;i++){
+            if(input_catagory=="" && input_author==""){
+                content=content+'<div class="blog">'+blog[i].innerHTML+'</div>'
+            }
+            else if(input_catagory=="" && input_author !=""){
+
+            }
+            else if(input_catagory!="" && input_author==""){
+                if(blog[i].children[0].children[1].children[2].innerHTML==input_catagory){
+                    content=content+'<div class="blog">'+blog[i].innerHTML+'</div>'
+                }
+            }
+            else{
+                
+            }
+            
+        }
+        blogs.innerHTML=content
+    }
 })
