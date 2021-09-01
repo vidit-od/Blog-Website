47ed054c70d7be3b6203dacff2ab7bfee39294d8
VIDIT <vidit894@gmail.com>
2021-09-01 11:58:24 +0530

    Fix menu button

--- static/app_blog.js
+++ static/app_blog.js
@@ -3,13 +3,11 @@ const catagory=document.querySelectorAll(".catagory")
 const catagory_select=document.querySelector(".catagory_select")
 let catagory_list=[]
 const form=document.querySelector(".form")
-const submit=document.querySelector(".submit")
 const blog=document.querySelectorAll(".blog")
 const blogs=document.querySelector(".blogs")
 const scroll_to_top=document.querySelector(".scroll_to_top")
 const pages=document.querySelector(".page")
 
-const open_menu=document.getElementById("menu")
 // loading content on page
 window.addEventListener('DOMContentLoaded',function(){
     // from the list of blogs, find all the unique catagories of blog and save it in catagory_list
@@ -47,7 +45,6 @@ window.addEventListener('DOMContentLoaded',function(){
         pages.innerHTML=content
 
         let current_page=parseInt(window.location.pathname.slice(12,))
-        console.log(current_page)
         // lower limit :((current_page-1)*max_blogs)+1
         // upper limit :current_page*max_blogs
 
@@ -59,59 +56,7 @@ window.addEventListener('DOMContentLoaded',function(){
         }
         blogs.innerHTML=page_content
     }
-
-    console.log(document.getElementById("menu"))
 })
-
- // for submit button of filter menu
- submit.addEventListener("click",function(){
-     // taking all the data according to which we will filter
-     const input_catagory= document.getElementById("catagory_select").value
-     const input_author=document.getElementById("author").value
-     let content=""
-     let loop = 0
-     // const blogs contains a list of all blogs in latest to oldest order
-     // if person wants latest to top order then we will traverse list from 0 to end
-     // total 4 catagories possible, filter nothing, filter acc to catagory , filter acc to author , filter acc to both
-     // using if and else if to find correct catagory and adding its inner html in content
-     // in end if content empty means no blog found according to the filter asked....so display no content
-     // else if content is not empty ... use its code as new inner html
-         for(i=0;i<blog.length;i++){
-             if(input_catagory=="" && input_author==""){
-                 loop=1
-                 content=content+'<div class="blog">'+blog[i].innerHTML+'</div>'
-             }
-             else if(input_catagory=="" && input_author !=""){
-                 loop=2
-                 if(blog[i].children[0].children[1].children[0].innerHTML==input_author){
-                     content=content+'<div class="blog">'+blog[i].innerHTML+'</div>'
-                 }
-             }
-             else if(input_catagory!="" && input_author==""){
-                 loop=3
-                 if(blog[i].children[0].children[1].children[2].innerHTML==input_catagory){
-                     content=content+'<div class="blog">'+blog[i].innerHTML+'</div>'
-                 }
-             }
-             else{
-                 loop=4
-                if((blog[i].children[0].children[1].children[2].innerHTML==input_catagory) && (blog[i].children[0].children[1].children[0].innerHTML==input_author)){
-                 content=content+'<div class="blog">'+blog[i].innerHTML+'</div>'
-                }
-             }
-            
-         }
-         if (content==""){
-             content=`<h3> No Results Found For this Filter </h3>`
-         }
-         if (loop==1){
-             document.querySelector(".section-title").innerHTML=' Recently Published'
-         }
-         else{
-             document.querySelector(".section-title").innerHTML=' Filtered Results'
-         }
-         blogs.innerHTML=content
- })
 
 // appear a scrool to top button if scrolled below certain level
 window.addEventListener("scroll",function(){
@@ -130,7 +75,6 @@ document.getElementById("close").addEventListener("click",function(){
 })
 
 // to open menu bar
-open_menu.addEventListener("click",function(){
-    console.log("hi")
+document.querySelector(".menu-btn").addEventListener("click",function(){
     document.querySelector(".menu").classList.add("toggle")
 })
