ac84decca9af784cc37e23ae6f008486df5d5a9e
VIDIT <vidit894@gmail.com>
2021-09-03 11:16:57 +0530

    User model temp setup

--- Post/admin.py
+++ Post/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import post,comment,catagory
+from .models import post,comment,catagory,users
 # Register your models here.
 admin.site.register(post)
 admin.site.register(comment)
 admin.site.register(catagory)
+admin.site.register(users)
--- Post/models.py
+++ Post/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class post(models.Model):
@@ -29,10 +30,20 @@ class catagory(models.Model):
         return '%s' %(self.catagory_name)
 
 class users(models.Model):
+    GENDER_choice={
+        ('male',"male"),
+        ("Female","female"),
+        ("others","Others")
+    }
+    
+    
     first_name=models.CharField(max_length=100,blank=True)
     last_name=models.CharField(max_length=100,blank=True)
-    user_id=models.CharField(max_length=100,blank=False,null=False)
-    gender=models.CharField(max_length=20,blank=True)
+    user_id=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    gender=models.CharField(max_length=20,blank=True,choices=GENDER_choice)
     age=models.IntegerField()
     profile_pic=models.ImageField(default="images\profile_pic\default-avatar.jpg", upload_to="images/profile_pic/")
 
+    
+    def __str__(self):
+        return '%s' %(self.user_id)
