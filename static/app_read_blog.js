f715f3e3d5de3f6809146300a250c5c578a28d39
VIDIT <vidit894@gmail.com>
2021-09-07 10:50:26 +0530

    Adiing comments to read_blog and its static files

--- static/app_read_blog.js
+++ static/app_read_blog.js
@@ -1,10 +1,12 @@
+// declaration
 const menu_btn=document.getElementById("menu-btn")
 const menu=document.querySelector('.menu')
 
+// open menu btn
 menu_btn.addEventListener("click",function(){
     menu.classList.add('toggle')
 })
-
+// close menu btn
 document.getElementById("close").addEventListener("click",function(){
     document.querySelector(".menu").classList.remove("toggle")
 })
